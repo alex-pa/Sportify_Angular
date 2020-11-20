9b8124ed46238295b9980d9b2558238e51f7ce5f
unknown <vipalexpa@gmail.com>
2020-11-20 13:37:02 +0200

    commit 5

--- sportify/src/app/login-form/login-form.component.ts
+++ sportify/src/app/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  navigateToFeed() {
+    this.router.navigate(['/feed']);
+ }
 
 }
